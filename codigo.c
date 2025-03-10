1d39479ee6e3ea19b7a4ac1632beaa4a4ea2fb97
luisprogramador01 <dinizluisfilipe@gmail.com>
2025-03-10 15:06:34 +0000

    novo printf

---
+++ codigo.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(){
+    int populacao;
+    int turismo;
+    float pib;
+    float area; 
+    char estado;
+    char codigo[3];
+    char cidade[50];
+
+    printf("Carta 1 \n");
+
+    printf("Nome da Cidade: Sorocaba \n");
+
+    printf("População: 500.000 \n");
+
+    printf("Pontos turísticos: 20 \n");
+
+    printf("Pib: 20.000.000 \n");
+
+    printf("Área em Km²: 500.000 \n");
+
+    printf("Estado: SP \n");
+
+    printf("Código da carta: A01 \n");
+
+    printf("\n");
+   
+    printf("Carta 2 \n");
+
+    printf("Cidade: Munique \n");
+
+    printf("População: 1.500.000 \n");
+
+    printf("Pontos Turísticos: 10 \n");
+
+    printf("PIB: 40.000.000 \n");
+
+    printf("Área em Km²: 320.10 \n");
+
+    printf("Estado: Baviera \n");
+
+    printf("Código da carta: A02 \n");
+
+    return 0;
+}
